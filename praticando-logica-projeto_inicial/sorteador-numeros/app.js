9d72075646fd37774b3e61d673fff287ccf8cb74
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-24 13:53:21 -0300

    add function sortearNumeroAleatorio

--- praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,6 +1,12 @@
 function sortear() {
-    let quantidade = document.getElementById("quantidade").value;
-    let de = document.getElementById("de").value;
-    let ate = document.getElementById("ate").value;
-    
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+    let numero = sortearNumeroAleatorio(de, ate);
+
+    console.log(numero);
+}
+
+function sortearNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
