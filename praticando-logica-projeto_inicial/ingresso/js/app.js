9768dbe30e68522f9153b670785bc6ed632fc065
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-26 18:25:39 -0300

    finish the exercise

--- praticando-logica-projeto_inicial/ingresso/js/app.js
+++ praticando-logica-projeto_inicial/ingresso/js/app.js
@@ -5,6 +5,14 @@ function comprar() {
     if(ingresso == "pista") {
         comprarPista(qtd);
     }
+
+    if(ingresso == "superior") {
+        comprarSuperior(qtd);
+    }
+
+    if(ingresso == "inferior") {
+        comprarInferior(qtd);
+    }
 }
 
 function comprarPista(qtd) {
@@ -12,8 +20,30 @@ function comprarPista(qtd) {
     if(qtd > qtdPista) {
         alert("Quandidade indisponível para pista");
     } else {
-        qtdPista = qtdPista - qtd;
+        qtdPista -= qtd;
         document.getElementById("qtd-pista").textContent = qtdPista;      
+        alert("Compra realizada com sucesso");
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById("qtd-superior").textContent);
+    if(qtd > qtdSuperior) {
+        alert("Quandidade indisponível para piso superior");
+    } else {
+        qtdSuperior -= qtd;
+        document.getElementById("qtd-superior").textContent = qtdSuperior;      
+        alert("Compra realizada com sucesso");
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById("qtd-inferior").textContent);
+    if(qtd > qtdInferior) {
+        alert("Quandidade indisponível para piso inferior");
+    } else {
+        qtdInferior -= qtd;
+        document.getElementById("qtd-inferior").textContent = qtdInferior;      
         alert("Compra realizada com sucesso");
     }
 }
