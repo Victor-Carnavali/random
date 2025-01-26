d70938042a19063cc8d1dfa926b5c22bec69323e
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-26 15:27:05 -0300

    finish the button adicionar()

--- praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,3 +1,7 @@
+let totalGeral = 0;
+document.getElementById("lista-produtos").innerHTML = "";
+document.getElementById("valor-total").innerHTML = "R$0";
+
 function adicionar() {
     // recuperar valores: nome produto, quantidade e valor
     let produto = document.getElementById("produto").value;
@@ -9,7 +13,12 @@ function adicionar() {
     produtosCarrinhos.innerHTML += `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`;
-    
+    totalGeral += preco;
+    let campoTotal = document.getElementById("valor-total");
+    campoTotal.textContent = `R$${totalGeral}`;
+    document.getElementById("quantidade").value = 0;
 }
 
-function limpar() {}
+function limpar() {
+
+}
