a71ed3e781fa7e7e56951de6547ac945e233b8f6
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-27 10:27:37 -0300

    add function to verify numeric digits in a string

--- praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -1,10 +1,24 @@
-function adicionar() {
-    let nomeAmigo = document.getElementById("nome-amigo").value;
-    let listaAmigos = document.getElementById("lista-amigos");
+let nomeAmigo = document.getElementById("nome-amigo").value;
+let lista = document.getElementById("lista-amigos");
+let listaArrayAmigos = [];
 
-    let amigosIncluidos = [];
-    
-    alert(nomeAmigo);
+function adicionar() {
+    nomeAmigo = document.getElementById("nome-amigo").value;
+    lista = document.getElementById("lista-amigos");
+
+    // /[\d]/ -> verifica se ha qualquer digito numerico (\d) no string.
+    // .test(nomeAmigo) -> retorna 'true' se houver numeros na string, e 'false' caso contrario.
+    if(!isNaN(nomeAmigo) || nomeAmigo.trim() == "" || /[\d]/.test(nomeAmigo)) {
+        alert("insira um nome válido");
+    } else {
+        listaArrayAmigos.push(nomeAmigo);
+        if(lista.textContent == "") {
+            lista.textContent = nomeAmigo;
+        } else {
+            lista.textContent += ", " + nomeAmigo;
+        }
+    }
+    nomeAmigo = "";
 }
 
 function sortear() {
