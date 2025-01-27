75d87198050bfec4d922dac726a00a8096667a8c
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-27 09:39:13 -0300

    create structure functions adicionar(), sortear() and  reiniciar()

---
+++ praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -0,0 +1,12 @@
+function adicionar() {
+    let nomeAmigo = document.getElementById("nome-amigo").value;
+    let listaAmigos = document.getElementById("lista-amigos");
+
+    let amigosIncluidos = [];
+    
+    alert(nomeAmigo);
+}
+
+function sortear() {
+
+}
